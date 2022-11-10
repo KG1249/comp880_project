e2218ab0491e66c5128a1f88046b1c816398901b
John <johnevans096@gmail.com>
2022-11-10 14:41:28 -0500

    change in line 4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def add(x, y):
     return x + y
 
-actual_value = add(3, 4)
+actual_result = add(3, 4)
 expected_result = 7
-assert (actual_value == expected_result)
+assert (actual_result == expected_result)
